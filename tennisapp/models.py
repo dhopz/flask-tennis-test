0a1b051e8efc3528f5b8dd767468d65574dd7e11
dhopz <david_houpapa@hotmail.com>
2022-03-07 17:09:09 +0000

    player validations added for age and uniqueness

--- tennisapp/models.py
+++ tennisapp/models.py
@@ -3,16 +3,19 @@ from . import db
 class PlayerModel(db.Model):
     __tablename__ = 'players'
 
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer,primary_key=True)
     first_name = db.Column(db.String())
     last_name = db.Column(db.String())
     nationality = db.Column(db.String())
     date_of_birth = db.Column(db.Date())
     points = db.Column(db.Integer(),default=1200)
+    __table_args__ = (db.UniqueConstraint('last_name', 'first_name', name='player_name_uc'),)
+    #player_name = db.column(db.String(),primary_key=True)
 
     def __init__(self, first_name, last_name, nationality, date_of_birth):
         self.first_name = first_name
         self.last_name = last_name
         self.nationality = nationality
         self.date_of_birth = date_of_birth
+
        
--- tennisapp/routes.py
+++ tennisapp/routes.py
@@ -1,40 +1,56 @@
 """Application routes."""
-from datetime import datetime
+#from datetime import datetime
+import datetime
+from sqlalchemy import exc
 
 from flask import current_app as app
 from flask import request, jsonify, json
 
 from .models import PlayerModel, db
 
+def age(dob):
+    today = datetime.date.today()
+    years = today.year - dob.year
+    if today.month < dob.month or (today.month == dob.month and today.day < dob.day):
+        years -= 1
+    return years
+
+
 @app.route('/')
 def hello():
 	return {"hello": "world"}
 
-
 @app.route('/players', methods=['POST', 'GET'])
 def handle_players():
-    if request.method == 'POST':
-        data = json.loads(request.data)        
-        new_player = PlayerModel(                
-            first_name=data['first_name'],
-            last_name=data['last_name'],
-            nationality=data['nationality'],
-            date_of_birth=datetime.strptime(data['date_of_birth'], '%d/%m/%Y')            
-            )
+    try:
+        if request.method == 'POST':
+            data = json.loads(request.data)
 
-        db.session.add(new_player)
-        db.session.commit()
+            if age(datetime.datetime.strptime(data['date_of_birth'], '%d/%m/%Y')) >= 16:
+                new_player = PlayerModel(                
+                    first_name=data['first_name'],
+                    last_name=data['last_name'],
+                    nationality=data['nationality'],
+                    date_of_birth=datetime.datetime.strptime(data['date_of_birth'], '%d/%m/%Y')            
+                    )
+                db.session.add(new_player)
+                db.session.commit()
+                return {"message": f"Player {new_player.first_name} {new_player.last_name} has been created successfully."}
+            else:
+                return {"error": "The Player needs to be at least 16"}
 
-        return {"message": f"Player {new_player.first_name} {new_player.last_name} has been created successfully."}
 
-    elif request.method == 'GET':
-        players = PlayerModel.query.all()
-        results = [
-            {
-                "first_name": player.first_name,
-                "last_name": player.last_name,
-                "nationality": player.nationality,
-                "date_of_birth":player.date_of_birth
-            } for player in players]
+        elif request.method == 'GET':
+            players = PlayerModel.query.all()
+            results = [
+                {
+                    "first_name": player.first_name,
+                    "last_name": player.last_name,
+                    "nationality": player.nationality,
+                    "date_of_birth":player.date_of_birth
+                } for player in players]
 
-        return {"count": len(results), "Players": results, "message": "success"}
+            return {"count": len(results), "Players": results, "message": "success"}    
+    
+    except exc.IntegrityError:
+        return {"error": "The Player has already been registered"}
